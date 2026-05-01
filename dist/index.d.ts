cf7a319598da7236547a5645b81c99e9954e79b2
Francisco <public@francisco.io>
2026-05-01 23:16:37 +0200

    Updated workflow

---
+++ dist/index.d.ts
@@ -0,0 +1,88 @@
+type Store = {
+    get: (key: string) => Promise<any>;
+    set: (key: string, value: any, options?: any) => Promise<any>;
+    del: (key: string) => Promise<any>;
+    has?: (key: string) => Promise<boolean>;
+    clear?: () => Promise<any>;
+};
+type Headers = {
+    [name: string]: string;
+};
+type Query = {
+    [name: string]: string;
+};
+type Methods = "get" | "head" | "post" | "patch" | "put" | "delete" | "GET" | "HEAD" | "POST" | "PATCH" | "PUT" | "DELETE";
+type Body = string | any[] | {
+    [key: string]: any;
+} | FormData | HTMLFormElement | SubmitEvent | ReadableStream;
+type FchError = Error & {
+    response?: Response;
+};
+type FchRequest = {
+    url: string;
+    method: string;
+    headers: Headers;
+    body?: string | FormData | ReadableStream | null;
+    credentials?: RequestCredentials;
+    signal?: AbortSignal;
+    [key: string]: any;
+};
+type FchResponse = {
+    status: number;
+    statusText: string;
+    headers: Headers;
+    body: any;
+};
+type Options = {
+    url?: string;
+    method?: Methods;
+    query?: Query;
+    headers?: Headers;
+    baseUrl?: string;
+    baseURL?: string;
+    cache?: Store;
+    output?: string;
+    credentials?: RequestCredentials;
+    before?: (req: FchRequest) => FchRequest | Promise<FchRequest>;
+    after?: (res: FchResponse) => FchResponse | Promise<FchResponse>;
+    error?: (error: FchError) => any;
+    signal?: AbortSignal;
+    [key: string]: any;
+};
+interface FchInstance {
+    <T = any>(url?: string, options?: Options): Promise<T>;
+    create: (options?: Options) => FchInstance;
+    get: <T = any>(url: string, options?: Options) => Promise<T>;
+    head: <T = any>(url: string, options?: Options) => Promise<T>;
+    post: <T = any>(url: string, body?: Body, options?: Options) => Promise<T>;
+    patch: <T = any>(url: string, body?: Body, options?: Options) => Promise<T>;
+    put: <T = any>(url: string, body?: Body, options?: Options) => Promise<T>;
+    delete: <T = any>(url: string, options?: Options) => Promise<T>;
+    del: <T = any>(url: string, options?: Options) => Promise<T>;
+    text: () => Promise<string>;
+    json: <T = any>() => Promise<T>;
+    blob: () => Promise<Blob>;
+    stream: () => ReadableStream | null;
+    arrayBuffer: () => Promise<ArrayBuffer>;
+    formData: () => Promise<FormData>;
+    body: <T = any>() => Promise<T>;
+    clone: () => Response;
+    raw: () => Response;
+    response: <T = any>() => Promise<T>;
+    url: string;
+    method: Methods;
+    query: Query;
+    headers: Headers;
+    baseUrl: string | null;
+    baseURL: string | null;
+    cache: Store | null;
+    output: string;
+    credentials: RequestCredentials;
+    before?: (req: FchRequest) => FchRequest | Promise<FchRequest>;
+    after?: (res: FchResponse) => FchResponse | Promise<FchResponse>;
+    error?: (error: FchError) => any;
+}
+declare function create(defaults?: Options): FchInstance;
+export { create };
+declare const _default: FchInstance;
+export default _default;
